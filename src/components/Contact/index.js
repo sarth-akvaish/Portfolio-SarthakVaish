ca3ba8700685ccb37e8f5958da8e1f3d95a95e0d
Sarthak <sarthakvkumar@gmail.com>
2024-04-14 12:00:08 +0530

    fixed changes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -146,14 +146,15 @@ const Contact = () => {
       <Wrapper>
         <Title>Contact</Title>
         <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
-        <ContactForm ref={form} onSubmit={handleSubmit}>
+        {/* <ContactForm ref={form} onSubmit={handleSubmit}>
           <ContactTitle>Email Me 🚀</ContactTitle>
           <ContactInput placeholder="Your Email" name="from_email" />
           <ContactInput placeholder="Your Name" name="from_name" />
           <ContactInput placeholder="Subject" name="subject" />
           <ContactInputMessage placeholder="Message" rows="4" name="message" />
           <ContactButton type="submit" value="Send" />
-        </ContactForm>
+        </ContactForm> */}
+        
         <Snackbar
           open={open}
           autoHideDuration={6000}
