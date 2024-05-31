3a275846037d96dcaa6c763a9a80c3fb48215814
Sarthak Vaish <108884904+sarth-akvaish@users.noreply.github.com>
2024-05-31 23:54:05 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sarth-akvaish",
   resume:
-    "https://drive.google.com/file/d/1oFeM2iS57crB0NIgsRyvMm-kMx0Te-X1/view?usp=sharing",
+    "https://drive.google.com/file/d/1Sa5FsNOARvlvr4Q2VbKArY-g6UkZWuW-/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/sarthakvaish",
   twitter: "https://twitter.com/unknown_sarthak",
   insta: "https://www.instagram.com/vaish_sa_rthak/",
